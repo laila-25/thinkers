682b9fea04bdd0384fc934e47aff8a90f86fdd94
laila-25 <marakshilaila@gmail.com>
2026-07-28 04:00:18 +0300

    Fix Laravel compiled view path

--- backend/config/view.php
+++ backend/config/view.php
@@ -1,16 +1,12 @@
-﻿<?php
-
-use Illuminate\Support\Facades\Facade;
+<?php
 
 return [
-
     'paths' => [
         resource_path('views'),
     ],
 
     'compiled' => env(
         'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
+        storage_path('framework/views')
     ),
-
 ];
